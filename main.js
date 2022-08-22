c8d63cbed6a5c99f12c0b0f9d1cf1470fc9dc4fd
MX <dinosaurbao@pm.me>
2022-08-21 21:05:28 -0400

    basic functions are added to the calculator

--- main.js
+++ main.js
@@ -4,16 +4,19 @@ var input = document.querySelector('input[type="text"]');
 var clear = document.querySelector('.clear');
 var operators = document.querySelectorAll('.operator');
 
+
 var number1 ="";
 var number2 = "";
 var operator = "";
 var displayItem = "";
+var number = "";
 
 function init(){
     clear.addEventListener('click', ()=>{
         number1 ="";
         number2 ="";
         displayItem ="";
+        number = "";
         input.value = '0';
     });
 }
@@ -26,32 +29,71 @@ function display(){
         button.addEventListener('click', (e)=>{
 
             if(e.target.className == "clear"){
-                
-                input.value = 0;
+            
+                displayItem = "0";
                 init();
             }
 
             else if (e.target.className == "number"){
-                displayItem += e.target.value;
+                if(input.value === "0"){
+                    displayItem = e.target.value;
+                }
+                else{
+                    displayItem += e.target.value; 
+                }
+              
+                number += e.target.value;
+              
+            }
+
+            else if(e.target.className == "decimal"){
+                if(!number.includes(".")){
+                    displayItem += e.target.value;
+                    number += e.target.value;
+                }
+                
+                
             }
 
             else if(e.target.className == "operator"){
               
-                number1 = displayItem;
+                if(number1 === ""){
+                    number1 = number;
+                    number = "";
+                }
+                else if(number1 !== "" && operator !== ""){
+                    number2 = number;
+                    number = "";
+                    
+                    const result = operate(operator, number1, number2);
+                    number1 = result;
+                    displayItem =  result;
+                    operator = e.target.value;
+                }
                 operator = e.target.value;
                 displayItem += operator;
             }
+            
 
-            else if(e.target.className == "="){
-                const result = operate(operator, number1, number2);
-                displayItem = result;
-
+            else if(e.target.className == "equal-sign"){
+                if(operator !== "" && number !==""){
+                    number2 = number;
+                    number = "";
+                    const result = operate(operator, number1, number2);
+                    
+                    operator ="";
+                    number1 = result;
+                    displayItem = result;
+                }
+               
             }
 
 
             console.log(e.target);
             //displayItem += e.target.value;
             console.log(number1);
+            console.log(number2);
+            console.log(operator);
              
             input.value = displayItem;
         })
@@ -84,20 +126,28 @@ const divide = function(a, b){
     }
 }
 
-console.log(multiply(-3, -21));
+const mod = function(a, b){
+    return a % b;
+}
+
+console.log(mod(21, 3));
 
 function operate(operator, number1, number2){
-    if(operator == "+"){
-        return add(parseInt(number1), parseInt(number2));
+    if(operator == " + "){
+        return add(parseFloat(number1), parseFloat(number2));
     }
-    else if(operator == "-"){
-        return subtract(parseInt(number1), parseInt(number2));
+    else if(operator == " - "){
+        return subtract(parseFloat(number1), parseFloat(number2));
     }
-    else if(operator == "×"){
-        return multiply(parseInt(number1), parseInt(number2));
+    else if(operator == " × "){
+        return multiply(parseFloat(number1), parseFloat(number2));
     }
-    else if(operator == "÷"){
-        return divide(parseInt(number1), parseInt(number2));
+    else if(operator == " ÷ "){
+        return divide(parseFloat(number1), parseFloat(number2));
+    }
+
+    else if(operator == " % "){
+        return mod(parseFloat(number1), parseFloat(number2));
     }
 
 }
@@ -106,5 +156,5 @@ function operate(operator, number1, number2){
 
 
 
-const result = operate("×", "12", "3");
+const result = operate(" × ", "12" , "3");
 console.log(result);
